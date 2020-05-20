daf256bb47e2f0e34505a3fd8fafa67c50f55945
Hayk Sargsyan <hayk.gukasyan@bk.ru>
2020-05-20 17:32:36 +0400

    first lesson

---
+++ go_lang_01/01.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := []float64{1, 2, 3, 4, 5, 6, 7}
+	n := 3
+	fmt.Print(sum(array))
+	fmt.Print(evenOrOdd(n))
+	fff := oddNumGenerator()
+	fmt.Print(fff())
+	fmt.Print(fibonachi(5))
+
+}
+
+func sum(arr []float64) float64 {
+	total := 0.0
+	for _, item := range arr {
+		total += item
+	}
+	return total
+}
+
+func evenOrOdd(n int) (int, bool) {
+	half := n / 2
+	if half%2 == 0 {
+		return 1, true
+	} else {
+		return 0, false
+	}
+}
+
+func oddNumGenerator() func() uint {
+	num := uint(1)
+	return func() (req uint) {
+		req = num
+		num += 2
+		return
+	}
+}
+
+func fibonachi(n int) int {
+	if n <= 1 {
+		return n
+	} else {
+		return fibonachi(n-1) + fibonachi(n-2)
+	}
+}
+
+
+
+
